6873c2db4af94105fcd37a27c0a14c25710ddf3f
niiallen <Allen.aryeetey@st.vvu.edu.gh>
2017-04-12 18:08:33 +0100

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -16,15 +16,7 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-//Route for add new task
 
-//Route::get('/add_task',function(){
-//    return view('add');
-//});
-//Route::post('/add_task',function(){
-//
-//   return "about to add";
-//});
 Route::resource('/add_task','TaskController');
 
 Auth::routes();
